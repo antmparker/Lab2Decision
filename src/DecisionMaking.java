c6f4ad780de17f2bb05c30298635f596fb71da4b
Anthony Parker <anthonymparker1@yahoo.com>
2017-02-01 13:41:54 -0500

    Revised

--- src/DecisionMaking.java
+++ src/DecisionMaking.java
@@ -9,20 +9,25 @@ public class DecisionMaking {
         Scanner scan = new Scanner(System.in);
 
         int userNum;
-        System.out.print("Enter a whole number between 1 and 100: ");
-        userNum = scan.nextInt();
 
-        if ((userNum >= 1) && (userNum <= 100) && (userNum % 2 != 0)) {
-            System.out.println(userNum + " and Odd.");
-        } else if ((userNum >= 2) && (userNum <= 25) && (userNum % 2 == 0)) {
-            System.out.println("Even and less than 25. ");
-        } else if ((userNum >= 26) && (userNum <= 60) && (userNum % 2 == 0)) {
-            System.out.println("Even. ");
-        } else if ((userNum > 60) && (userNum % 2 == 0)) {
-            System.out.println(userNum + " Even. ");
-        } else if ((userNum > 60) && (userNum % 2 != 0)) {
-            System.out.println(userNum + " and Odd. ");
+        while (true) {
+            System.out.print("Enter a whole number between 1 and 100: ");
+            userNum = scan.nextInt();
+            if ((userNum >= 1) && (userNum <= 100)) {
+                if ((userNum >= 1) && (userNum <= 60) && (userNum % 2 != 0)) {
+                    System.out.println("Odd.");
+                } else if ((userNum >= 2) && (userNum < 25) && (userNum % 2 == 0)) {
+                    System.out.println("Even and less than 25. ");
+                } else if ((userNum >= 26) && (userNum <= 60) && (userNum % 2 == 0)) {
+                    System.out.println("Even. ");
+                } else if ((userNum > 60) && (userNum % 2 == 0)) {
+                    System.out.println(userNum + " and Even. ");
+                } else if ((userNum > 60) && (userNum % 2 != 0)) {
+                    System.out.println(userNum + " and Odd. ");
+                }
+            }
         }
-        scan.close();
+
+
     }
 }
